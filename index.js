94e4a5d07c904a01bd09f750c7a5e9d0f71ede58
Tushar Popli <tusharpopli1406@gmail.com>
2022-03-18 23:38:58 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require("express");
 const res = require("express/lib/response");
 const app=express();
 const data=require('./data.json');
+var cors = require('cors')
 let port=process.env.PORT||3000;
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send(data);
